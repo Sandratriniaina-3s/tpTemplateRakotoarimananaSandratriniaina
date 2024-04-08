c4563afd231465c9863d4955511e5a422fb8c16a
Sandratra <3s.sandratra@gmail.com>
2024-04-08 21:42:16 +0300

    Etat initial (TP2)

---
+++ src/main/java/mg/itu/tptemplaterakotoarimananasandratriniaina/config/ConfigJSF.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tptemplaterakotoarimananasandratriniaina.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Configuration JSF
+ *
+ * @author Sandratriniaina
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+
+}
